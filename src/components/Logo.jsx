c0fef9ffdd0b6f0cdccae08a903f1bdaa6ad7c85
houyembrichni <brichnihouyem908@gmail.com>
2024-03-05 10:17:09 +0100

    updating

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,8 +4,9 @@ import { NavLink } from "react-router-dom";
 import "./NavBarStyle.css";
 import image from '../assets/logo.png'
 function Logo() {
-  return (
+  return ( 
     <NavLink to="/" className="navbar-logo">
+      <title className="title"> Stroy Room</title>
       <img src={image} alt="" />
     </NavLink>
   );
